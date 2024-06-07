d67073bba0f26a759a6d111aae4a75c8a66d0860
thiago <thiago_gja27@hotmail.com>
2024-06-07 20:44:52 -0300

    componetizando a aplicação

---
+++ src/components/Formulario.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(['cadastrarTarefa','tarefaTemp', 'trocarFiltro','editaTarefaTemp']);
+</script>
+
+
+
+
+<template>
+    <form @submit.prevent="props.cadastrarTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required
+                    type="text" placeholder="Digite a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                    <option value="todas">Todas Tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps(['tarefas']);
+</script>
+
+
+<template>
+    <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa.titulo">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <p v-else class="mt-3" style="color: red;"> Não existem tarefas pendentes</p>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
